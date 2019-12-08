974ab35b10cc9c1f5791ef221e53e4c4adccd46d
J.Heilmann <joachim.h@posteo.de>
2019-12-08 16:16:44 +0100

    2nd try :-/

--- src/main/java/kernbeisser/Windows/ManageUser/ManageUserUIView.java
+++ src/main/java/kernbeisser/Windows/ManageUser/ManageUserUIView.java
@@ -1,4 +1,19 @@
 package kernbeisser.Windows.ManageUser;
 
-public class ManageUserUIView {
+import kernbeisser.CustomComponents.ObjectTable.Column;
+import kernbeisser.CustomComponents.ObjectTable.ObjectTable;
+
+import javax.swing.*;
+
+public class ManageUserUIView extends JPanel {
+    private JPanel mainPanel;
+    private ObjectTable<User> userTable = new ObjectTable<>(
+            Column.create("Username", User::getUsername),
+            Column.create("Vorname", User::getFirstName),
+            Column.create("Nachname", User::getSurname)
+    );
+    private JButton addUser;
+    private JButton editUser;
+    private JButton deleteUser;
+    private JButton close;
 }
