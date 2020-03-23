0a51f13a25f77f2d281715d74d5dacde9b92625a
groump <julikiller98@gmail.com>
2020-03-23 19:37:57 +0100

    fixed VAT low

--- src/main/java/kernbeisser/Enums/VAT.java
+++ src/main/java/kernbeisser/Enums/VAT.java
@@ -3,9 +3,8 @@ package kernbeisser.Enums;
 import kernbeisser.Useful.Named;
 
 public enum VAT implements Named {
-    LOW("Niedrig(7%)",Setting.VAT_HIGH.getDoubleValue()),
+    LOW("Niedrig(7%)",Setting.VAT_LOW.getDoubleValue()),
     HIGH("Hoch(19%)",Setting.VAT_HIGH.getDoubleValue())
-
     ;
 
     private final String name;
