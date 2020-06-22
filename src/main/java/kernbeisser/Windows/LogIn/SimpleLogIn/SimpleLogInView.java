c105a1cb2e8baab269febe450910dadbc0d27021
heintzl <joachim.h@posteo.de>
2020-06-22 23:34:15 +0200

    added timeout Timer to BarcodeCapture with Setting.SCANNER_TIMEOUT ms and moved activateKeyboardListener from SimpleLogInView to Main

--- src/main/java/kernbeisser/Windows/LogIn/SimpleLogIn/SimpleLogInView.java
+++ src/main/java/kernbeisser/Windows/LogIn/SimpleLogIn/SimpleLogInView.java
@@ -58,7 +58,6 @@ public class SimpleLogInView implements View<SimpleLogInController> {
                 Tools.showUnexpectedErrorWarning(e);
             }
         }
-        Tools.activateKeyboardListener();
         // TODO test code; remove  up to here
     }
 
