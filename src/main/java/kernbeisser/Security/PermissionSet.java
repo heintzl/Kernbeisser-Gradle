24c7c6611ee31dfb35ba7948edf6264fd502285f
groump <julikiller98@gmail.com>
2020-06-28 13:26:20 +0200

    started to comment Security Classes

--- src/main/java/kernbeisser/Security/PermissionSet.java
+++ src/main/java/kernbeisser/Security/PermissionSet.java
@@ -6,9 +6,20 @@ import kernbeisser.Enums.Key;
 import java.util.Arrays;
 import java.util.Collection;
 
+/**
+ * loads given permission set into a bit field saved
+ * as long array so we can easily check a permission
+ * with bit field comparision against it.
+ */
+
 public final class PermissionSet {
     private static final long[] bits = new long[((Key.values().length / 64) + 1)];
 
+
+    /**
+     * loads all permission from the collection into the bit field
+     * @param permissions the collection of all permission which the logged in user has.
+     */
     public static void loadPermission(Collection<Permission> permissions){
         Arrays.fill(bits,0L);
         for (Permission permission : permissions) {
@@ -19,16 +30,30 @@ public final class PermissionSet {
         }
     }
 
+    /**
+     * checks if the PermissionSet contains a Key by the ordinal value of it
+     * @param key the key which should get tested against the bit field
+     * @return the boolean if the PermissionSet contains the permission
+     */
     public static boolean hasPermission(Key key){
         long bit = (1<<(key.ordinal()%64));
         return (bits[key.ordinal() / 64] & bit) == bit;
     }
 
+
+    /**
+     * sets specific key in the PermissionSet to true
+     * @param key the key which should added to the permission set
+     */
     public static void addPermission(Key key){
         int o = key.ordinal();
         bits[o / 64] = (bits[o / 64] | (1<<(o%64)));
     }
 
+    /**
+     * removes the Key from the permission
+     * @param key the key which should be removed
+     */
     public static void removePermission(Key key){
         int o = key.ordinal();
         long bit =  (1<<(key.ordinal()%64));
@@ -36,6 +61,13 @@ public final class PermissionSet {
         bits[o / 64] = (bits[o / 64] & (1<<(o%64)));
     }
 
+
+    /**
+     * {@link #hasPermission(Key)}
+     * checks if the PermissionSet contains multiple keys
+     * @param keys all keys in a var args form
+     * @return true if the PermissionSet contains all keys
+     */
     public static boolean hasPermissions(Key ... keys){
         for (Key key : keys) {
             if(!hasPermission(key))return false;
--- src/main/java/kernbeisser/Security/Proxy.java
+++ src/main/java/kernbeisser/Security/Proxy.java
@@ -20,6 +20,14 @@ import java.util.Arrays;
 import java.util.Collection;
 
 public class Proxy {
+
+    /**
+     * wraps a Object with a Proxy which is used to check if the PermissionSet contains the required Keys to
+     * run the Function else it throws a AccessDeniedException
+     * @param parent the Object which should get wrap into a Proxy Object
+     * @param <T> the type of the Object
+     * @return a Proxy which extends T
+     */
     public static <T> T getSecureInstance(T parent){
         if (ProxyFactory.isProxyClass(parent.getClass()))return parent;
         ProxyFactory factory = new ProxyFactory();
@@ -34,6 +42,13 @@ public class Proxy {
         }
     }
 
+    /**
+     * do the same like getSecureInstance only for a whole collection
+     * @param collection collection  with Objects
+     * @param <C> the collection with the values
+     * @param <V> the type of the Object which gets transformed
+     * @return the collection c with Proxy extends v values
+     */
     public static <C extends Collection<V>,V> C getSecureInstances(C collection){
         if(collection.size()==0)return collection;
         V any = collection.iterator().next();
@@ -54,10 +69,22 @@ public class Proxy {
         return collection;
     }
 
+
+    /**
+     * returns if an Object is a ProxyInstance
+     * @param o the object
+     * @return if the object is a ProxyInstance
+     */
     public static boolean isProxyInstance(Object o) {
         return ProxyFactory.isProxyClass(o.getClass());
     }
 
+
+    /**
+     * reutns the SecurityHandler of an specified object
+     * @param o the ProxyInstance
+     * @return the SecurityHandler from the object
+     */
     @SneakyThrows
     public static SecurityHandler getHandler(Object o){
         Field handler = o.getClass().getDeclaredField("handler");
@@ -65,6 +92,12 @@ public class Proxy {
         return (SecurityHandler) handler.get(o);
     }
 
+    /**
+     * the class which handel when a method is called by a proxy instance
+     * SecurityHandler checks if the current load PermissionSet contains all keys
+     * which specified in Key annotation to run the method when it doesn't than
+     * the security handler throws a AccessDeniedException
+     */
     static class SecurityHandler implements MethodHandler {
         public Object invoke(Object proxy, Method proxyMethod,Method original, Object[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, AccessDeniedException
         {
@@ -73,7 +106,7 @@ public class Proxy {
             if(key==null || PermissionSet.hasPermissions(key.value()))
                     out = original.invoke(proxy, args);
                 else throw new AccessDeniedException("User["+LogInModel.getLoggedIn().getId() + "] cannot access " + original + " because the user has not the required Keys:" + Arrays.toString(key.value()));
-            return out;
+            return Proxy.getSecureInstance(out);
         }
     }
 }
