765b944332b1db1d35b22188bc57509a1dc5d50a
groump <julikiller98@gmail.com>
2020-07-04 15:00:47 +0200

    applied changes

--- src/main/java/kernbeisser/DBEntities/Shelf.java
+++ src/main/java/kernbeisser/DBEntities/Shelf.java
@@ -29,27 +29,4 @@ public class Shelf {
     @Setter(onMethod_= {@Key(PermissionKey.SHELF_ARTICLES_WRITE)})
     private Set<Article> articles = new HashSet<>();
 
-    public int getId() {
-        return id;
-    }
-
-    public void addArticle(Article a){
-        articles.add(a);
-    }
-
-    public void removePriceList(PriceList p){
-        articles.remove(p);
-    }
-
-    public Set<Article> getArticles() {
-        return articles;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
