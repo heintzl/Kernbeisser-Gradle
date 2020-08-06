7616cf5ef49a3c46c04228d95fd70ba94457c14e
groump <julikiller98@gmail.com>
2020-08-06 23:46:57 +0200

    fixed lost selection when refresh db content

--- src/main/java/kernbeisser/CustomComponents/AccessChecking/ObjectValidator.java
+++ src/main/java/kernbeisser/CustomComponents/AccessChecking/ObjectValidator.java
@@ -2,6 +2,6 @@ package kernbeisser.CustomComponents.AccessChecking;
 
 import kernbeisser.Exeptions.CannotParseException;
 
-public interface ObjectValidator <T> {
-    T validate(T input) throws CannotParseException;
+public interface ObjectValidator<T> {
+  T validate(T input) throws CannotParseException;
 }
