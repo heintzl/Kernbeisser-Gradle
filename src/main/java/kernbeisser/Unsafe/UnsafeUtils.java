74181f64e7bb0d127a6ad52ee8379df7a2c2ccde
groump <julikiller98@gmail.com>
2020-09-13 19:31:19 +0200

    added UnsafeUtils to setClass from Object via ObjectHeader

---
+++ src/main/java/kernbeisser/Unsafe/UnsafeUtils.java
@@ -0,0 +1,31 @@
+package kernbeisser.Unsafe;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class UnsafeUtils {
+    public static sun.misc.Unsafe unsafe = create();
+
+    public static <T> T setClass(Object o,Class<T> clazz) throws InstantiationException {
+        Object patternObject = unsafe.allocateInstance(clazz);
+        unsafe.getAndSetInt(o,8,unsafe.getInt(patternObject,8));
+        return (T) o;
+    }
+
+    private static sun.misc.Unsafe create(){
+        Field field;
+        try {
+            field = Unsafe.class.getDeclaredField("theUnsafe");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException();
+        }
+        field.setAccessible(true);
+        try {
+            return (sun.misc.Unsafe) field.get(null);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
