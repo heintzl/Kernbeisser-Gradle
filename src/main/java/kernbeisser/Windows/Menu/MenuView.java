b2d9c1c04cb2936c36a48f524f45b6ea24a8209c
heintzl <joachim.h@posteo.de>
2021-03-28 14:43:17 +0200

    added user preorder button, rename user group

--- src/main/java/kernbeisser/Windows/Menu/MenuView.java
+++ src/main/java/kernbeisser/Windows/Menu/MenuView.java
@@ -53,7 +53,7 @@ public class MenuView implements IView<MenuController> {
   private ControllerButton editArticles;
   private ControllerButton editSurchargeTables;
   private kernbeisser.CustomComponents.ControllerButton changePassword;
-  private kernbeisser.CustomComponents.ControllerButton transactionHistory;
+  private kernbeisser.CustomComponents.ControllerButton openSelfPreorder;
   private kernbeisser.CustomComponents.ControllerButton showUserInfo;
   private kernbeisser.CustomComponents.ControllerButton editUserSettings;
   private ControllerButton editUsers;
@@ -113,11 +113,6 @@ public class MenuView implements IView<MenuController> {
             () -> new ChangePasswordController(LogInModel.getLoggedIn(), true),
             ChangePasswordController.class,
             Controller::openTab);
-    transactionHistory =
-        new ControllerButton(
-            () -> new UserInfoController(LogInModel.getLoggedIn()),
-            UserInfoController.class,
-            Controller::openTab);
     showUserInfo =
         new ControllerButton(
             () -> new UserInfoController(LogInModel.getLoggedIn()),
@@ -157,7 +152,9 @@ public class MenuView implements IView<MenuController> {
     editJobs = new ControllerButton(EditJobs::new, EditJobs.class, Controller::openTab);
     editApplicationSettings =
         new ControllerButton(SettingController::new, SettingController.class, Controller::openTab);
-    order = new ControllerButton(PreOrderController::new, PreOrderController.class);
+    order = new ControllerButton(() -> new PreOrderController(false), PreOrderController.class);
+    openSelfPreorder =
+        new ControllerButton(() -> new PreOrderController(true), PreOrderController.class);
     adminTools = new ControllerButton(AdminToolController::new, AdminToolController.class);
     // NOT IMPLEMENTED
     placeHolderControllerButton2 = ControllerButton.empty();
