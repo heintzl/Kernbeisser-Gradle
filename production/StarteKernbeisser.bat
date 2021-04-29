fe7410cd77548638c04f36404383812f60236b8e
heintzl <joachim.h@posteo.de>
2021-04-29 22:53:27 +0200

    add java agent to starter

--- production/StarteKernbeisser.bat
+++ production/StarteKernbeisser.bat
@@ -1,1 +1,1 @@
-start javaw -jar kernbeisser.jar
+java "-javaagent:Agent-1.0-SNAPSHOT.jar=kernbeisser$kernbeisser.Security.Key$kernbeisser.Security.Access.Access.hasAccess(this,\"%%s\", \"%%s\", %%dL);" -jar kernbeisser.jar
