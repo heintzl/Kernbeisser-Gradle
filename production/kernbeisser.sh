bba3d309acbc9f99fba0b132ce6d42a035d8588d
groump <julikiller98@gmail.com>
2021-09-12 13:10:11 +0200

    fixed nulllptr. on linux only by a work around

--- production/kernbeisser.sh
+++ production/kernbeisser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java "-javaagent:Agent-1.0-SNAPSHOT.jar=kernbeisser\$kernbeisser.Security.Key\$kernbeisser.Security.Access.Access.hasAccess(this, \"%s\", \"%s\", %dL);" -jar kernbeisser.jar
+java "-javaagent:Agent-1.0-SNAPSHOT.jar=kernbeisser\$kernbeisser.Security.Key\$kernbeisser.Security.Access.Access.hasAccess(this, \"%s\", \"%s\", %dL);" -jar kernbeisser-1.0-all.jar
