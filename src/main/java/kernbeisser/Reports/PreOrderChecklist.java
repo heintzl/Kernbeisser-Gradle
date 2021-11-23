bde301196609d7d6cc505ecf159a790f3f693fd9
heintzl <joachim.h@posteo.de>
2021-11-23 23:46:07 +0100

    improve preorderchecklist according to JK

--- src/main/java/kernbeisser/Reports/PreOrderChecklist.java
+++ src/main/java/kernbeisser/Reports/PreOrderChecklist.java
@@ -3,14 +3,19 @@ package kernbeisser.Reports;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
 import kernbeisser.DBEntities.PreOrder;
+import kernbeisser.DBEntities.User;
 
 public class PreOrderChecklist extends Report {
   private final Collection<PreOrder> preorder;
 
   public PreOrderChecklist(Collection<PreOrder> preorder) {
     super("preOrderChecklist", "preOrderChecklist" + LocalDate.now().toString());
-    this.preorder = preorder;
+    this.preorder =
+        preorder.stream()
+            .filter(p -> !p.getUser().equals(User.getKernbeisserUser()))
+            .collect(Collectors.toList());
   }
 
   @Override
