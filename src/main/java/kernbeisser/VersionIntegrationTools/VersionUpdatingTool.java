f407013adf39881a11a54ce4dd7973b4a297601f
heintzl <joachim.h@posteo.de>
2022-01-02 13:50:01 +0100

    String templates for easy addition or rename of Permissionkey

--- src/main/java/kernbeisser/VersionIntegrationTools/VersionUpdatingTool.java
+++ src/main/java/kernbeisser/VersionIntegrationTools/VersionUpdatingTool.java
@@ -3,4 +3,10 @@ package kernbeisser.VersionIntegrationTools;
 public interface VersionUpdatingTool {
 
   void runIntegration();
+
+  String renameKeySetQuery = "UPDATE Permission_keySet SET keySet = :to WHERE keySet = :from";
+  String insertRwKeySetPairQuery =
+      "INSERT INTO Permission_keySet (Permission_id, keySet) "
+          + "SELECT Permission_id, REPLACE(keySet, :template, :new) "
+          + "FROM Permission_keySet WHERE keySet LIKE CONCAT(:template, '_%')";
 }
