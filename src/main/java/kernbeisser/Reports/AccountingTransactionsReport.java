0d1df19e7f3f37a91922703c72ea3f2bf8f67414
heintzl <joachim.h@posteo.de>
2022-03-13 20:29:50 +0100

    add UserGroup value sums at given transaction to accounting transaction report if not printed in accounting report

--- src/main/java/kernbeisser/Reports/AccountingTransactionsReport.java
+++ src/main/java/kernbeisser/Reports/AccountingTransactionsReport.java
@@ -1,12 +1,10 @@
 package kernbeisser.Reports;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import kernbeisser.DBEntities.Transaction;
 import kernbeisser.DBEntities.User;
+import kernbeisser.DBEntities.UserGroup;
 import kernbeisser.Exeptions.NoTransactionsFoundException;
 
 public class AccountingTransactionsReport extends Report {
@@ -14,9 +12,10 @@ public class AccountingTransactionsReport extends Report {
   private final long reportNo;
   private final List<Transaction> transactions;
   private final boolean withNames;
+  private final boolean printValueSums;
 
   public AccountingTransactionsReport(
-      long reportNo, List<Transaction> transactions, boolean withNames)
+      long reportNo, List<Transaction> transactions, boolean withNames, boolean printValueSums)
       throws NoTransactionsFoundException {
     super(
         "accountingTransactionReportFileName",
@@ -24,14 +23,18 @@ public class AccountingTransactionsReport extends Report {
     this.reportNo = reportNo;
     this.transactions = transactions;
     this.withNames = withNames;
+    this.printValueSums = printValueSums;
   }
 
   @Override
   Map<String, Object> getReportParams() {
-    Map<String, Object> reportParams = new HashMap<>();
+    Transaction lastTransaction =
+        transactions.stream().max(Comparator.comparingLong(Transaction::getId)).get();
+    Map<String, Object> reportParams = UserGroup.getValueAggregatesAtReportNo(reportNo);
     reportParams.put("userGroup", User.getKernbeisserUser().getUserGroup());
     reportParams.put("reportNo", reportNo);
     reportParams.put("reportTitle", AccountingReport.getReportTitle(reportNo, transactions));
+    reportParams.put("printValueSums", printValueSums);
     return reportParams;
   }
 
