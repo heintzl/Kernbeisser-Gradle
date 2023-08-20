efbb33759257a5d8774bedbd3fa6d871c7da0f64
heintzl <joachim.h@posteo.de>
2023-08-20 19:25:42 +0200

    generic reader for complete List of DBEntities

--- src/main/java/kernbeisser/Windows/EditCatalog/EditCatalogModel.java
+++ src/main/java/kernbeisser/Windows/EditCatalog/EditCatalogModel.java
@@ -1,17 +1,11 @@
 package kernbeisser.Windows.EditCatalog;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import kernbeisser.DBConnection.DBConnection;
 import kernbeisser.DBEntities.CatalogEntry;
 import kernbeisser.Windows.MVC.IModel;
-import lombok.Cleanup;
 import lombok.Getter;
 
 public class EditCatalogModel implements IModel<EditCatalogController> {
@@ -19,16 +13,7 @@ public class EditCatalogModel implements IModel<EditCatalogController> {
   @Getter private final Collection<CatalogEntry> catalog;
 
   public EditCatalogModel() {
-    catalog = getQuery();
-  }
-
-  private static List<CatalogEntry> getQuery() {
-    @Cleanup EntityManager em = DBConnection.getEntityManager();
-    CriteriaBuilder cb = em.getCriteriaBuilder();
-    CriteriaQuery<CatalogEntry> cr = cb.createQuery(CatalogEntry.class);
-    cr.select(cr.from(CatalogEntry.class));
-    TypedQuery<CatalogEntry> q = em.createQuery(cr);
-    return q.getResultList();
+    catalog = DBConnection.getAll(CatalogEntry.class);
   }
 
   // Ignores the Max rows setting
