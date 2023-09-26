ec78c1df77128cc43b98c4f171b3bd2f7537bd69
heintzl <joachim.h@posteo.de>
2023-09-26 21:27:09 +0200

    display comparison warning

--- src/main/java/kernbeisser/Windows/Supply/SupplySelector/SupplySelectorController.java
+++ src/main/java/kernbeisser/Windows/Supply/SupplySelector/SupplySelectorController.java
@@ -15,7 +15,6 @@ import kernbeisser.DBEntities.Article;
 import kernbeisser.DBEntities.Articles;
 import kernbeisser.DBEntities.ShoppingItem;
 import kernbeisser.DBEntities.Supplier;
-import kernbeisser.Enums.ArticleConstants;
 import kernbeisser.Enums.Mode;
 import kernbeisser.Enums.Setting;
 import kernbeisser.Exeptions.PermissionKeyRequiredException;
@@ -225,20 +224,6 @@ public class SupplySelectorController extends Controller<SupplySelectorView, Sup
             supply.getAllLineContents(),
             "Kornkraft Obst und Gemüse Verkaufspreise vom " + supply.getDeliveryDate())
         .sendToPrinter("Drucke Obst und Gemüse Verkaufspreise", Tools::showUnexpectedErrorWarning);
-  }
-
-  public static Article wrapToPrint(LineContent content) {
-    Article article =
-        Articles.getByKbNumber(ArticleConstants.PRODUCE.getUniqueIdentifier(), false)
-            .map(e -> e.getValue())
-            .orElse(new Article());
-    article.setSuppliersItemNumber(content.getKkNumber());
-    article.setSupplier(Supplier.getKKSupplier());
-    article.setMetricUnits(content.getUnit());
-    article.setWeighable(true);
-    article.setName(content.getName());
-    article.setNetPrice(content.getPriceKk());
-    return article;
   }
 
   public void viewOrders(ActionEvent actionEvent) {
