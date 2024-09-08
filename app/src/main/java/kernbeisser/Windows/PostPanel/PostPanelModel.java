bc157e4d744105dbf080d35f3eca4b68a4ab00ef
heintzl <joachim.h@posteo.de>
2024-09-08 15:02:41 +0200

    fixed a couple of runtime Exceptions and introduces standard Popup size closes #789

--- app/src/main/java/kernbeisser/Windows/PostPanel/PostPanelModel.java
+++ app/src/main/java/kernbeisser/Windows/PostPanel/PostPanelModel.java
@@ -1,8 +1,13 @@
 package kernbeisser.Windows.PostPanel;
 
+import java.awt.*;
 import kernbeisser.DBEntities.Post;
 import kernbeisser.Enums.PostContext;
+import kernbeisser.Enums.Setting;
+import kernbeisser.Exeptions.handler.UnexpectedExceptionHandler;
+import kernbeisser.Useful.Tools;
 import kernbeisser.Windows.MVC.IModel;
+import rs.groump.AccessDeniedException;
 
 public class PostPanelModel implements IModel<PostPanelController> {
 
@@ -29,7 +34,11 @@ public class PostPanelModel implements IModel<PostPanelController> {
   }
 
   public void setPostActive(boolean active) {
-    post = post.setActive(active);
+    try {
+      post = post.setActive(active);
+    } catch (AccessDeniedException e) {
+      UnexpectedExceptionHandler.showErrorWarning(e, "Fehlende Berechtigung");
+    }
   }
 
   public void saveContent(String htmlContent, int width, int height) {
@@ -37,10 +46,16 @@ public class PostPanelModel implements IModel<PostPanelController> {
   }
 
   public int getPopupWidth() {
-    return post.getPopupWidth();
+    return Tools.ifNull(
+        post.getPopupWidth(),
+        Math.round(Setting.SUBWINDOW_SIZE_FACTOR.getFloatValue())
+            * Toolkit.getDefaultToolkit().getScreenSize().width);
   }
 
   public int getPopupHeight() {
-    return post.getPopupHeight();
+    return Tools.ifNull(
+        post.getPopupHeight(),
+        Math.round(Setting.SUBWINDOW_SIZE_FACTOR.getFloatValue())
+            * Toolkit.getDefaultToolkit().getScreenSize().height);
   }
 }
