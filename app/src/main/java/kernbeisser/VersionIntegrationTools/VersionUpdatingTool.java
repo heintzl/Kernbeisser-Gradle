6d516cda1a79f8c223772222381828f01bb2a519
heintzl <joachim.h@posteo.de>
2025-04-07 22:24:50 +0200

    new Enum value TransactionType.SHARED_CONTAINER

--- app/src/main/java/kernbeisser/VersionIntegrationTools/VersionUpdatingTool.java
+++ app/src/main/java/kernbeisser/VersionIntegrationTools/VersionUpdatingTool.java
@@ -1,5 +1,15 @@
 package kernbeisser.VersionIntegrationTools;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import kernbeisser.DBConnection.DBConnection;
+import lombok.Cleanup;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface VersionUpdatingTool {
 
   void runIntegration();
@@ -13,4 +23,18 @@ public interface VersionUpdatingTool {
       "INSERT INTO Permission_keySet (Permission_id, keySet) "
           + "SELECT Permission_id, :new "
           + "FROM Permission_keySet WHERE keySet = :template";
+
+  default void updateEnum(List<String> enumNames, String table, String column) {
+    String sql = "ALTER TABLE %s MODIFY COLUMN %s ENUM(%s)".formatted(
+            table,
+            column,
+            enumNames.stream().map("'%s'"::formatted).collect(Collectors.joining(", ")));
+    @Cleanup EntityManager em = DBConnection.getEntityManager();
+    @Cleanup("commit")
+    EntityTransaction et = em.getTransaction();
+    et.begin();
+    em.createNativeQuery("SET SESSION alter_algorithm='INSTANT';").executeUpdate();
+    em.createNativeQuery(sql).executeUpdate();
+
+  }
 }
