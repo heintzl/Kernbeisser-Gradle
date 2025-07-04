2cc86ef7b29d18f4804db627c507360f75f8e3ca
heintzl <joachim.h@posteo.de>
2025-07-04 16:01:25 +0200

    remove automatic export of user balances (#835 AK 4)

--- app/src/main/java/kernbeisser/Windows/CashierShoppingMask/CashierShoppingMaskModel.java
+++ app/src/main/java/kernbeisser/Windows/CashierShoppingMask/CashierShoppingMaskModel.java
@@ -1,19 +1,14 @@
 package kernbeisser.Windows.CashierShoppingMask;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import kernbeisser.DBEntities.Repositories.TransactionRepository;
 import kernbeisser.DBEntities.Transaction;
 import kernbeisser.DBEntities.User;
-import kernbeisser.Enums.Setting;
 import kernbeisser.Exeptions.InvalidReportNoException;
 import kernbeisser.Exeptions.NoTransactionsFoundException;
 import kernbeisser.Exeptions.handler.UnexpectedExceptionHandler;
 import kernbeisser.Reports.AccountingReport;
-import kernbeisser.Reports.UserBalanceReport;
 import kernbeisser.Windows.MVC.IModel;
 import lombok.Data;
 
@@ -27,7 +22,6 @@ public class CashierShoppingMaskModel implements IModel<CashierShoppingMaskContr
 
   public static int printAccountingReports() {
     try {
-      AtomicBoolean success = new AtomicBoolean(true);
       List<Transaction> unreportedTransactions = TransactionRepository.getUnreportedTransactions();
       if (unreportedTransactions.isEmpty()) {
         return 0;
@@ -37,22 +31,6 @@ public class CashierShoppingMaskModel implements IModel<CashierShoppingMaskContr
           .exportPdfToCloud(
               "Erstelle Buchhaltungsbericht",
               UnexpectedExceptionHandler::showUnexpectedErrorWarning);
-
-      Instant lastUserBalance = Instant.parse(Setting.LAST_USER_BALANCE_REPORT.getStringValue());
-      if (lastUserBalance
-          .plus(Setting.USER_BALANCE_REPORT_INTERVAL.getIntValue(), ChronoUnit.DAYS)
-          .isBefore(Instant.now())) {
-        new UserBalanceReport(no, true)
-            .exportPdfToCloud(
-                "Kontostände werden exportiert",
-                (e) -> {
-                  success.set(false);
-                });
-        if (success.get()) {
-          Setting.LAST_USER_BALANCE_REPORT.changeValue(
-              Instant.now().truncatedTo(ChronoUnit.DAYS).toString());
-        }
-      }
       return 0;
     } catch (NoTransactionsFoundException e) {
       return 0;
