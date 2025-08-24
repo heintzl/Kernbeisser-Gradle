afa9ed80e2d6ba7357acea6c0157ed33c36ddb67
heintzl <joachim.h@posteo.de>
2025-08-24 17:33:32 +0200

    refactoring: most logic ist rewritten and much more efficient: constructor is privatized, old transaction based parameters are calculated from DTOs instead, purchase based parameters are derived in one efficient loop VATExceptions are handled in MVCs

--- app/src/main/java/kernbeisser/Exeptions/InvalidVATValueException.java
+++ app/src/main/java/kernbeisser/Exeptions/InvalidVATValueException.java
@@ -1,7 +1,9 @@
 package kernbeisser.Exeptions;
 
+import kernbeisser.Enums.VAT;
+
 public class InvalidVATValueException extends Exception {
-  public InvalidVATValueException(double vat) {
-    super(vat * 100 + "% is no valid VAT rate");
+  public InvalidVATValueException(VAT vat, double value) {
+    super("%f.2 is no valid VAT rate for %s".formatted(value * 100, vat.getName()));
   }
 }
